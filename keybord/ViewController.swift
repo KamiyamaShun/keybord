7ce367f5741f2ec194c9cbab570da57b81f1835a
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-02-10 11:44:16 +0900

    set keyboard function

--- keybord/ViewController.swift
+++ keybord/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        userName.delegate = self
+        password.delegate = self
     }
 
-
+    @IBAction func signUp(_ sender: Any) {
+        logoImage.image = UIImage(named: "loginOK")
+        userNameLabel.text = userName.text
+        passwordLabel.text = password.text
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
